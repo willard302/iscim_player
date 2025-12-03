aee289bd89cb4170fe00060472a141ed40cc99f5
Nico Wu <>
2025-12-03 11:03:01 +0800

    update:新增多語言

--- i18n/locales/en.ts
+++ i18n/locales/en.ts
@@ -7,6 +7,8 @@ export default {
   introduce: "introduce",
   introduce_article: "The human brain consists of 100 billion neurons connected by 100 trillion nerves. When large numbers of neurons send synchronized pulses, they oscillate and form wave-like brain waves. The shape of brain waves is closely related to the operation of our body's various systems, daily routines, and circadian rhythms. iSCIM Music Therapy produces special curative frequency music according to the daily climate rhythm to help you maintain the correct balance of brain waves so that you can present your best every day.",
   iscim_music: "iSCIM Music Player",
+  setting: "Setting",
+  user_data: "User Data",
   Chakra: {
     Overall: "Overall Chakra",
     Balance: "Balance Chakra",
