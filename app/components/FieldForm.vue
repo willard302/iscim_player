9f234edc50735b24acb9aebf911991996491716b
Nico Wu <>
2025-12-13 17:56:46 +0800

    component:新增component FieldForm

---
+++ app/components/FieldForm.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+import type { ButtonItem, FieldItem, FieldOption } from '~/types/data.types';
+const { t } = useI18n();
+
+const props = defineProps<{
+  fieldItems?: FieldItem[],
+  buttonItems?: ButtonItem[],
+  customClass?: string,
+  buttonClass?: string
+}>();
+
+const emit = defineEmits(['submit', 'button', 'passwordToggle']);
+
+const fields = ref(props.fieldItems);
+
+const showPicker = ref(false);
+const pickerOptions = ref<any[]>([]);
+const pickerField = ref<FieldItem | null>(null);
+const labelWidth = '6em';
+
+const handleOnClick = (event: ButtonItem) => {
+  if (event.to) return navigateTo(event.to);
+  if (event.type === 'submit') return emit('submit', fields.value);
+  if (event.action) return emit('button', event.action);
+};
+
+const openPicker = (field: FieldItem) => {
+  pickerOptions.value = [];
+  switch(field.name) {
+    case "member_name":
+      pickerOptions.value = field.options?.map(f => ({text: f.text, value: f.text, id: f.id})) ?? [];
+      break;
+    default:
+      pickerOptions.value = field.options?.map(f => ({text: f.text, value: t(f.value)})) ?? [];
+      break;
+  };
+  pickerField.value = field;
+  showPicker.value = true;
+};
+
+interface SelectedType {
+  selectedIndexes: number[]
+  selectedOptions: FieldOption[]
+  selectedValues: string[]
+}
+
+const onConfirm = (
+  selected: SelectedType
+) => {
+  if (!pickerField.value) return;
+  pickerField.value.value = selected.selectedValues[0] ?? '';
+  showPicker.value = false;
+};
+
+const onCancel = () => {
+  showPicker.value = false;
+};
+
+const handleShowPassword = (type: string) => {
+  emit('passwordToggle', type);
+}
+</script>
+
+<template>
+  <van-form :class="props.customClass">
+    <van-cell-group v-if="fields" inset>
+      <template v-for="(field, fieldIdx) in fields" :key="fieldIdx">
+
+        <van-field
+          v-if="field.type === 'checkbox'"
+          :type="field.type"
+          :name="field.name"
+          :label="$t(field.label)"
+          :label-width="labelWidth"
+        >
+          <template v-if="field.type === 'checkbox'" #input>
+            <van-switch v-model="field.value" />
+          </template>
+        </van-field>
+
+        <van-field 
+          v-else-if="field.options" 
+          is-link
+          readonly
+          :name="field.name"
+          :label="$t(field.label)"
+          :label-width="labelWidth"
+          :placeholder="field.placeholder"
+          :model-value="String(field.value)"
+          @click="openPicker(field)"
+        />
+
+        <van-field
+          v-else-if="field.type"
+          :type="field.type"
+          v-model="field.value as string | number | undefined"
+          :name="field.name"
+          :label="$t(field.label)"
+          :label-width="labelWidth"
+          :placeholder="field.placeholder ? $t(field.placeholder) : ''"
+          :rules="[{
+            required: field.required,
+            message: field.message ? $t(field.message) : ''
+          }]"
+          :autocomplete="field.autocomplete"
+          @click-right-icon="field.name.includes('password') ? handleShowPassword(field.name) : null"
+        >
+          <template v-if="field.name.includes('password')" #right-icon>
+            <font-awesome :icon="['fas', (field.type === 'password' ? 'eye' : 'eye-slash')]" />
+          </template>
+        </van-field>
+      </template>
+    </van-cell-group>
+
+    <van-popup v-model:show="showPicker" round position="bottom">
+      <van-picker 
+        :columns="pickerOptions"
+        @confirm="onConfirm"
+        @cancel="onCancel"
+      />
+    </van-popup>
+
+    <div v-if="props.buttonItems" :class="['button__menu', props.buttonClass]">
+      <van-button 
+        v-for="(btnItem, btnIdx) in props.buttonItems"
+        :key="btnIdx"
+        round block
+        type="primary"
+        :native-type="btnItem.type"
+        @click="handleOnClick(btnItem)"
+      >
+        {{ $t(btnItem.text) }}
+      </van-button>
+    </div>
+  </van-form>
+</template>
+
+<style scoped lang="scss">
+
+.button__menu {
+  margin: 20px auto 10px;
+
+  .van-button {
+    margin: 10px auto;
+    max-width: 90%;
+  }
+}
+</style>
