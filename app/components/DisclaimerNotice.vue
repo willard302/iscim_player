606a2e8d859f1461b3265050d6d2830286865844
Nico Wu <>
2025-12-19 16:24:23 +0800

    components:優化免責條款套件

--- app/components/DisclaimerNotice.vue
+++ app/components/DisclaimerNotice.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <van-notice-bar 
-    class="disclaimer" 
-    background="transparent"
+  <van-notice-bar
     color="red"
     wrapable
   >
     《 {{$t("Disclaimer.title")}} 》 {{$t("Disclaimer.content")}}
   </van-notice-bar>
 </template>
-
-<style scoped lang="scss">
-  .disclaimer {
-    align-items: flex-start;
-  }
-</style>
