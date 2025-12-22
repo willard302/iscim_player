09985677d9d6c40032cc573e71c08a399f531794
Nico Wu <>
2025-12-22 17:23:08 +0800

    add:新增音樂模式切換功能

--- app/pages/userCenter/setting.vue
+++ app/pages/userCenter/setting.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { useMenuStore } from '~/store/useMenuStore';
+import { useMusicStore } from '~/store/useMusicStore';
 
-  const menuStore = useMenuStore();
+const menuStore = useMenuStore();
+const musicStore = useMusicStore();
+
 </script>
 
 <template>
   <van-cell-group>
+    <van-cell title="音樂模式" center>
+      <template #value>
+        <van-button size="small" @click="musicStore.handleToggleType()">
+          {{musicStore.isPro ? $t("Music.pro") : $t("Music.pub")}}
+        </van-button>
+      </template>
+    </van-cell>
     <van-cell title="點歌模式" center>
       <template #value>
         <van-button size="small" @click="menuStore.toggleMenu('listMode')">
