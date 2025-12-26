4774d69ee9fe1c83135edfea4f447abdce6e32bf
Nico Wu <>
2025-12-26 14:20:01 +0800

    composable:創建getMusics, getSets

--- app/composables/useDataBase.ts
+++ app/composables/useDataBase.ts
@@ -1,5 +1,5 @@
 import type { Database } from "~/types/database.types";
-import type { UserInsert, UserUpdate } from "~/types/supabase";
+import type { SetInsert, UserInsert, UserUpdate } from "~/types/supabase";
 
 export const useDataBase = () => {
 
@@ -22,7 +22,6 @@ export const useDataBase = () => {
     handleError(error, "Error fetching user")
     return data;
   };
-
   const insertUser = async(userInfo: UserInsert) => {
     const {data, error} = await client
       .from("users")
@@ -33,7 +32,6 @@ export const useDataBase = () => {
     handleError(error, `Error inserting user.`);
     return data;
   };
-
   const updateUser = async(userId: string, updateData: UserUpdate) => {
     const {data, error} = await client
       .from('users')
@@ -46,10 +44,47 @@ export const useDataBase = () => {
     return data;
   };
 
+  const getMusics = async(category?: string) => {
+
+    let query = client
+      .from("music_library")
+      .select("*");
+
+    if (category) {
+      query = query.eq("category", category);
+    };
+
+    const {data, error} = await query;
+
+    handleError(error, "Error fetching musics");
+    return data ?? []
+  };
+
+  interface MusicQueryOptions {
+    category?: string
+    is_pro?: boolean
+  };
+
+  const getSets = async(options: MusicQueryOptions = {}) => {
+    const {category, is_pro} = options;
+
+    let query = client.from("music_sets").select("*");
+
+    if (category) query = query.eq("category", category);
+    if (is_pro !== undefined) query = query.eq("is_pro", is_pro);
+
+    const {data, error} = await query;
+
+    handleError(error, "Error fetching sets");
+    return data as SetInsert[];
+  }
+
   return {
     getUser,
     insertUser,
-    updateUser
+    updateUser,
+    getMusics,
+    getSets
   }
 
 }
